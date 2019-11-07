01533e1feb580c5a61aaada023a7555d0edb2e67
jiangyh1233 <56244347+jiangyh1233@users.noreply.github.com>
2019-11-07 13:21:33 +0800

    🆗

---
+++ 计算器研发/计算器3.0/加减乘除.java
@@ -0,0 +1,64 @@
+ /**
+     * 处理运算符键被按下的事件
+     *
+     * @param key
+     */
+    private void handleOperator(String key) {
+        if (operator.equals("/")) {
+            // 除法运算
+            // 如果当前结果文本框中的值等于0
+            if (getNumberFromText() == 0.0) {
+                // 操作不合法
+                operateValidFlag = false;
+                resultText.setText("除数不能为零");
+            } else {
+                resultNum /= getNumberFromText();
+            }
+        } else if (operator.equals("1/x")) {
+            // 倒数运算
+            if (resultNum == 0.0) {
+                // 操作不合法
+                operateValidFlag = false;
+                resultText.setText("零没有倒数");
+            } else {
+                resultNum = 1 / resultNum;
+            }
+        } else if (operator.equals("+")) {
+            // 加法运算
+            resultNum += getNumberFromText();
+        } else if (operator.equals("-")) {
+            // 减法运算
+            resultNum -= getNumberFromText();
+        } else if (operator.equals("*")) {
+            // 乘法运算
+            resultNum *= getNumberFromText();
+        } else if (operator.equals("sqrt")) {
+            // 平方根运算
+            resultNum = Math.sqrt(resultNum);
+        } else if (operator.equals("%")) {
+            // 百分号运算，除以100
+            resultNum = resultNum / 100;
+        } else if (operator.equals("+/-")) {
+            // 正数负数运算
+            resultNum = resultNum * (-1);
+        } else if (operator.equals("=")) {
+            // 赋值运算
+            resultNum = getNumberFromText();
+        }
+        if (operateValidFlag) {
+            // 双精度浮点数的运算
+            long t1;
+            double t2;
+            t1 = (long) resultNum;
+            t2 = resultNum - t1;
+            if (t2 == 0) {
+                resultText.setText(String.valueOf(t1));
+            } else {
+                resultText.setText(String.valueOf(resultNum));
+            }
+        }
+        // 运算符等于用户按的按钮
+        operator = key;
+        firstDigit = true;
+        operateValidFlag = true;
+    }
