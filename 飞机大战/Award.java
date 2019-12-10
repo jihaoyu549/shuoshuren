3d30cb87d60688ed45daaff085314c6a5490c834
jiangyh1233 <56244347+jiangyh1233@users.noreply.github.com>
2019-12-10 08:52:59 +0800

    Add files via upload

---
+++ 飞机大战/Award.java
@@ -0,0 +1,11 @@
+package cn.tedu.shoot;
+
+/*
+ * 返回奖励类型接口
+ */
+public interface Award {
+	public int DOUBLE_FIRE = 0;
+	public int LIFE = 1;
+	public int getType();
+	
+}
---
+++ 飞机大战/Enemy.java
@@ -0,0 +1,8 @@
+package cn.tedu.shoot;
+
+/*
+ * 返回分数接口
+ */
+public interface Enemy {
+	public int getScore();
+}
